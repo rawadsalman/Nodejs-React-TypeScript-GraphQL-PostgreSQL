cb8d4705263b053eb150c86f61f27758f02d8cea
rawadsalman <rwadsalman81@gmail.com>
2021-06-20 01:52:36 +0200

    Update addone.ts

--- frontend/src/gqlQueries/companies/addone.ts
+++ frontend/src/gqlQueries/companies/addone.ts
@@ -1,7 +1,4 @@
 import gql from 'graphql-tag'
-import companyColumns from './company.columns'
-
-
    
  export const CREATE_COMPANY = gql`
   mutation createcompany($input: ShowCompanyViewModel!,$pathBuilder: any) {
@@ -9,88 +6,9 @@ import companyColumns from './company.columns'
        @rest(type: "ShowCompanyViewModel", path: "companies", pathBuilder: $pathBuilder, method: "POST") { 
      
       name,
-      addressBillingCity
+      addressBillingCity,
+      memberSince
 }
 }
 `    
-/* export const CREATE_COMPANY = gql`
-mutation Createcompany($input:ShowCompanyViewModel!,$bodyBuilder:any) {
-    Createcompany (input:$input,bodyBuilder:$bodyBuilder)
-       @rest(type: "ShowCompanyViewModel", path: "/companies", method: "POST",bodyBuilder:$bodyBuilder) { 
-      id
-      name
-      addressBillingCity
-
-}
-}
-`  */   
-/* export const CREATE_COMPANY = gql`
-mutation Createcompany {
-    Createcompany(input:§input) {
-      
-          id
-          name
-          addressBillingCity
-          
-      
-    }
-  }
-  ` */
-/*  export const CREATE_COMPANY = gql`
-mutation Createcompany($name: String!,$addressBillingCity:String!,$bodyBuilder:any) {
-    Createcompany (name: $name,addressBillingCity: $addressBillingCity)
-       @rest(type: "ShowCompanyViewModel", path: "/companies", method: "POST",bodyBuilder:$bodyBuilder) { 
-      id
-      name
-      addressBillingCity
-
-}
-}
-`   */
-/* export const CREATE_COMPANY = gql`
-mutation (
-    $input: ShowCompanyViewModel!
-    $bodyBuilder:any
-      ){
-    Createcompany(input: $input)
-       @rest(type: "ShowCompanyViewModel", path: "/companies", method: "POST",bodyBuilder: $bodyBuilder) { 
-      id
-      name
-      addressBillingCity
-
-}
-}
-` */
-/* export const CREATE_COMPANY = gql`
-mutation (
-    $someApiWithACustomBodyKey: PublishablePostInput!
-    $input: ShowCompanyViewModel!
-      ){
-    Createcompany(input: $input,body: $someApiWithACustomBodyKey)
-       @rest(type: "ShowCompanyViewModel", path: "/companies", method: "POST", bodyKey: "body") { 
-      id
-      name
-      addressBillingCity
-
-}
-}
-`
- */
-/* export const CREATE_COMPANY = gql`
-mutation Createcompany($name: String!,$addressBillingCity:String!) {
-    post (name: $name,addressBillingCity: $addressBillingCity){
-      id
-      name
-      addressBillingCity
-
-}
-}
-` */
-/* export const CREATE_COMPANY = gql`
-mutation Createcompany($name: String!,$addressBillingCity:String!,$uuid:ID!){ 
-    uuid
-    name
-    addressBillingCity
-}
-`   */
 export default CREATE_COMPANY;
