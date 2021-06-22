1f3be0a53a3822bb99fe86b42cbea609439603ae
rawadsalman <rwadsalman81@gmail.com>
2021-06-22 20:57:50 +0200

    adding getCompany and addcompany and deletecompany functions

--- backend/src/database/repository/company/company.repository.ts
+++ backend/src/database/repository/company/company.repository.ts
@@ -4,13 +4,10 @@ import companyColoums from './company.columns'
 
 export interface ICompanyModel extends ShowCompanyViewModel {
   id: string;
-  //name:string;
-  //addressBillingCity:string;
 }
 
 export default class CompanyRepository extends AbstractRepository {
   public static tableName = 'company'
-  // public static id : string;
   public CreateUUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -20,9 +17,10 @@ export default class CompanyRepository extends AbstractRepository {
   public getCompanies(): Promise< []> {
     return this.knex(CompanyRepository.tableName).select(companyColoums)
   }
-   /* public getCompany(): Promise< []> {
-    return this.knex(CompanyRepository.tableName).where('id', uuid).select('id','uuid','name')
-  }  */ 
+  public getCompany(uuid:string): Promise< [ICompanyModel]> {
+   console.log("knex uuid..",uuid)
+   return  this.knex(CompanyRepository.tableName).where('uuid', uuid).select('id','uuid', 'name', 'memberSince', 'addressBillingCity', 'addressBillingPhone');
+  }  
   public addCompany(name:string,addressBillingCity:string, memberSince:string): Promise< [ICompanyModel]> {
     return this.knex.insert({
     uuid:this.CreateUUID(),
